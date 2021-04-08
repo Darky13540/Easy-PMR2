a9a27da59a97dd6660ca014a0de43965d6ffeebc
Darky13540 <gibaud.chris@gmail.com>
2021-04-08 17:05:40 +0200

    2021.04.08 savee

--- details.php
+++ details.php
@@ -14,6 +14,5 @@ $reponse = $pdo->prepare('SELECT * FROM shop WHERE idi= ?');
 $reponse->execute([$_GET['id']]);
 $details = $reponse->fetch(PDO::FETCH_ASSOC);
 
-/* var_dump($_GET['id']);
-var_dump($details); */
+
 require ('views/details.phtml');
