7e27d912b5b656966b3b9c2fa4aa58ca7f381fea
Darky13540 <gibaud.chris@gmail.com>
2021-04-15 14:13:48 +0200

    inscription

---
+++ deconnexion.php
@@ -0,0 +1,9 @@
+<?php 
+session_start();
+ if(!isset($_SESSION['user'])){
+     header("Location: connexion.php");
+     exit();
+ }
+$_SESSION['user'] = [];
+unset($_SESSION['user']);
+header("Location: index.php");
