a724ddf62535f7d1bc07e944587340f31eb45975
Darky13540 <gibaud.chris@gmail.com>
2021-04-28 21:31:32 +0200

    2021.04.28 save

---
+++ bdconnect.php
@@ -0,0 +1,15 @@
+<?php
+
+/* Construction d'un objet PDO représentant la connexion à une BDD*/
+
+const DBNAME = 'darky13540_easy-pmr';
+const HOST = 'mysql-darky13540.alwaysdata.net';
+const LOGIN = '229228';
+const PASSWORD = 'Azertyuiop1#';
+
+$pdo = new PDO(
+   'mysql:host=' . HOST . ';dbname=' . DBNAME . ';charset=UTF8',
+   LOGIN,
+   PASSWORD
+); 
+
